112ab1fb17d09003b027e9570b4981be57fbbd46
Simon Smith <smithsps@gmail.com>
2016-04-24 22:44:33 -0400

    Added login form, and partial logic

--- server.py
+++ server.py
@@ -1,23 +1,27 @@
 from flask import Flask, render_template
+import flask.ext.login as flask_login
 app = Flask(__name__)
+
+login_manager = flask_login.LoginManager()
+login_manager.init_app(app);
+
 app.debug = True
 
 @app.route('/')
 def index():
     return render_template("labs.html")
 
-@app.route('/login')
+@app.route('/login', methods=['POST'])
 def login():
+    redirect('/');
+
+@app.route('/login')
+def login_page():
     return render_template("login.html")
 
 @app.route('/about')
 def about():
     return render_template("about.html")
-
-@app.after_request
-def cors(response):
-	response.headers['Access-Control-Allow-Origin'] = '*'
-	return response
 
 
 if __name__ == '__main__':
---
+++ user.py
@@ -0,0 +1,14 @@
+class User {
+    
+    def is_authenticated():
+        return False;
+        
+    def is_active():
+        return False;
+        
+    def is_anonymous():
+        return False;
+    
+    def get_id():
+        return False;
+}
