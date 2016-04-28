cd84a04509831488ad2c7c7ecea3699cd7cb30b5
Simon Smith <smithsps@gmail.com>
2016-04-28 14:48:39 -0400

    Added verification to the website.

--- server.py
+++ server.py
@@ -1,4 +1,6 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request, redirect
+import requests
+import json
 import flask.ext.login as flask_login
 app = Flask(__name__)
 
@@ -17,12 +19,21 @@ def login():
 
 @app.route('/login')
 def login_page():
-    return render_template("login.html")
+    redirect('/');
 
 @app.route('/about')
 def about():
     return render_template("about.html")
 
+@app.route('/verify/<username>')
+def verify(username):
+    req = requests.post('https://mc15.cs.purdue.edu:5001/user/verify/',
+                                verify='server.cer',
+                                data = json.dumps({'username': username,
+                                        'verify': request.args.get("v")
+                                        }))
+
+    return render_template("verify.html", status=req.status_code)
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=8000)
